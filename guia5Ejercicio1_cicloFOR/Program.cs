fd84e6afdb4a5d8ebc572f6f2bf97f582d744165
jfcava <juancavallieri@gmail.com>
2022-08-21 18:15:47 -0300

    C# - CICLO FOR - Muestro los numeros del 1 al 10

---
+++ guia5Ejercicio1_cicloFOR/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace guia5Ejercicio1_cicloFOR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Hacer un programa para mostrar los números del 1 al 10. 
+            // No se debe realizar ningún pedido de datos.
+            
+            Console.WriteLine("Inicio del programa");
+            for(int i = 1; i < 11; i++){
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
